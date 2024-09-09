110bd5e379eb2042aedd6f7ceeb886389a9cfc92
CozyLeprechaun94 <yousifrihmani@gmail.com>
2024-09-09 16:05:32 +0200

    gaat goed

--- src/main/java/nl/han/bewd/lambdas/Main.java
+++ src/main/java/nl/han/bewd/lambdas/Main.java
@@ -1,34 +1,52 @@
 package nl.han.bewd.lambdas;
 
-import nl.han.bewd.lambdas.reis.*;
-
-import java.util.Random;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
-    /*
-    Een Reis wordt gemaakt met een enkel vervoermiddel. De reistijd kan door middel van een simulatie van de
-    reis worden berekend. Hierbij kan onderscheid gemaakt worden of de reis tijdens de spits of buiten de spits is.
-     */
+    private final List<String> hobbits = Arrays.asList("Frodo", "Sam", "Merry", "Pippin");
+
     public static void main(String[] args) {
-        System.out.println("Buiten spits met fiets");
-        Reis mijnReis = new Reis(false);
-        VervoerStrategy vervoer = new FietsVervoerStrategy();
-        mijnReis.simuleerReis(vervoer);
+//        System.out.println("Buiten spits met fiets");
+//        Reis mijnReis = new Reis(false);
+//        VervoerStrategy vervoer = new FietsVervoerStrategy();
+//        mijnReis.simuleerReis(vervoer);
+//
+//        System.out.println("Tijdens de spits");
+//        mijnReis.setSpits(true);
+//        mijnReis.simuleerReis(vervoer);                   // Met fiets
+//        mijnReis.simuleerReis(new AutoVervoerStrategy()); // Met auto
+//        mijnReis.simuleerReis(new OVVervoerStrategy());   // Met OV
+//        mijnReis.simuleerReis(new PaardVervoerStrategy()); //Met paard
+//
+//        System.out.println("Met <nieuw> vervoer (Stap 2)  ");
+//        System.out.println("TODO:");
+//
+//        System.out.println("Met <nieuw> vervoer, lambda edition! (Stap 5)");
+//        System.out.println("TODO:");
+//
+//        VervoerStrategy teleportatieLambda = (isReisTijdensSpits) -> 0; //Lambda
+//        mijnReis.simuleerReis(teleportatieLambda); // Met teleportatieLambda object
 
-        System.out.println("Tijdens de spits");
-        mijnReis.setSpits(true);
-        mijnReis.simuleerReis(vervoer);                   // Met fiets
-        mijnReis.simuleerReis(new AutoVervoerStrategy()); // Met auto
-        mijnReis.simuleerReis(new OVVervoerStrategy());   // Met OV
-        mijnReis.simuleerReis(new PaardVervoerStrategy()); //Met paard
+        Main main = new Main();
+        main.run();
+    }
 
-        System.out.println("Met <nieuw> vervoer (Stap 2)  ");
-        System.out.println("TODO:");
+    public void run() {
 
-        System.out.println("Met <nieuw> vervoer, lambda edition! (Stap 5)");
-        System.out.println("TODO:");
+        hobbits.sort(Comparator.comparingInt(String::length)); //String::length is een methode refferentie. Syntaxic sugar voor een lambda expressie.
+        // Sorteer op lengte van de namen.
+        // Dit is een lambda expressie die een Comparator object maakt.
+        hobbits.forEach(System.out::println);
+    }
 
-        VervoerStrategy teleportatieLambda = (isReisTijdensSpits) -> 0; //Lambda
-        mijnReis.simuleerReis(teleportatieLambda); // Met teleportatieLambda object
+    public static class Scheduler implements Runnable {
+        @Override
+        public void run()
+        {
+            System.out.println("Scheduler is running");
+        }
     }
 }
